c15a618ac542c8367e7a7c4dbd2b1bb8c084cfd9
Margo Garber <margogarber@example.com>
2025-10-15 09:40:46 +0300

    Configure GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/FastSpot/', // Set this to your repository name
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
